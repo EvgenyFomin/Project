6e0d35806b72059e347a36d25b667e4ddf236857
EvgenyFomin <sevenup96@mail.ru>
2015-03-03 20:04:21 +0300

    fixMain

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         Fill f = new Fill();
+        ForUser user = new ForUser();
+        user.forUser();
         f.fill();
         f.output();
         f.Scan();
