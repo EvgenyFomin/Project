c60edcce19c7f89be8627ff7c3c958c54810a79b
EvgenyFomin <sevenup96@mail.ru>
2015-03-03 18:16:19 +0300

    updateFiles

--- Instructions.java
+++ Instructions.java
@@ -1,34 +1,30 @@
-import java.awt.datatransfer.StringSelection;
 
-/**
- * Created by wolfram on 21.02.15.
- */
 public class Instructions {
     boolean bool = true;
     public String Instructions(String s) {
         if (s.equals("u "))   {
             s = "U E' ";
-        }else if (s.equals("u' ")) {
+        } else if (s.equals("u' ")) {
             s = "U' E ";
-        }else if (s.equals("f ")){
+        } else if (s.equals("f ")) {
             s = "F S ";
-        }else if (s.equals("f' ")){
+        } else if (s.equals("f' ")) {
             s = "F' S' ";
-        }else if (s.equals("d ")){
+        } else if (s.equals("d ")) {
             s = "D E ";
-        }else if (s.equals("d' ")){
+        } else if (s.equals("d' ")) {
             s = "D' E' ";
-        }else if (s.equals("r ")){
+        } else if (s.equals("r ")) {
             s = "R M' ";
-        }else if (s.equals("r' ")){
+        } else if (s.equals("r' ")) {
             s = "R' M ";
-        }else if (s.equals("l ")){
+        } else if (s.equals("l ")) {
             s = "L M ";
-        }else if (s.equals("l' ")){
+        } else if (s.equals("l' ")) {
             s = "L' M' ";
-        }else if (s.equals("b ")){
+        } else if (s.equals("b ")) {
             s = "B S' ";
-        }else if (s.equals("b' ")){
+        } else if (s.equals("b' ")) {
             s = "B' S ";
         } else bool = false;
         return (s);
--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 
-/**
- * Created by wolfram on 21.02.15.
- */
 public class Main {
     public static void main(String[] args) {
-        //scan s = new scan();
-        //s.Scan();
+        scan s = new scan();
+        s.Scan();
         Fill f = new Fill();
         f.fill();
         f.output();
         f.Scan();
     }
 }
-
